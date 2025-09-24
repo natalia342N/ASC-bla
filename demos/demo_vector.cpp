9fe124b97944d0ccd99c0eef8d073e45d7cb8954
Natalia Tylek <e12332258@student.tuwien.ac.at>
2025-09-24 12:32:02 +0200

    expr added

--- demos/demo_vector.cpp
+++ demos/demo_vector.cpp
@@ -7,7 +7,7 @@ namespace bla = ASC_bla;
 
 int main()
 {
-  size_t n = 10;
+  size_t n = 5;
   bla::Vector<double> x(n), y(n);
 
   for (size_t i = 0; i < x.Size(); i++)
@@ -19,15 +19,4 @@ int main()
   bla::Vector<double> z = x+y;
   
   std::cout << "x+y = " << z << std::endl;
-  
-  std::cout << "type of (x+3*y) is  " << typeid(x+3*y).name() << std::endl;
-
-  std::cout << "x+3*y = " << x+3*y << std::endl;
-
-  std::cout << "sizeof(x+3*y) = " << sizeof(x+3*y) << std::endl;
-  
-  x.Range(2,9) = 3;
-  x.Slice(1,5) = 10;
-  
-  std::cout << "x = " << x << std::endl;  
 }
